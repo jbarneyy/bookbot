2e78557d32e7f3e70633cbd3b2fdb60c7f7e19b1
jbarneyy <js_barnett@outlook.com>
2025-05-29 18:03:43 -0700

    Completed BootDev Bookbot project.

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
-from stats import book_wc, char_count
+import sys
+from stats import book_wc, char_count, sorted_char_count
 
 
 # Main fuction logic.
 def main():
 
-    frankenstein_string = get_book_text("books/frankenstein.txt")
+    if (len(sys.argv) < 2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_fp = sys.argv[1]
+    book_string = get_book_text(book_fp)
     # print(frankenstein_string)
 
-    num_words = book_wc(frankenstein_string)
-    print(f"{num_words} words found in the document")
+    num_words = book_wc(book_string)
+    # print(f"Found {num_words} total words")
 
-    unique_char_counts = char_count(frankenstein_string)
-    print(f"{unique_char_counts}")
+    unique_char_counts = char_count(book_string)
+    # print(f"{unique_char_counts}")
 
+    sorted_list_of_chars = sorted_char_count(unique_char_counts)
+    # print(f"{sorted_list_of_chars}")
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_fp}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    for i in sorted_list_of_chars:
+        print(f"{i["char"]}: {i["num"]}")
+
+    print("============= END ===============")
 
 # Function for getting text from file and returning as String.
 def get_book_text(filepath):
--- stats.py
+++ stats.py
@@ -11,9 +11,29 @@ def char_count(book_as_string):
     characters = {}
 
     for c in book_as_string:
+
         if c in characters:
             characters[c] += 1
         else:
             characters[c] = 1
 
     return characters
+
+
+# Takes the dictionary of characters and returns a sorted list of dictionaries. Also filters so only alphabetical characters are included.
+def sorted_char_count(char_dict):
+
+    dict_list = []
+
+    for k in char_dict:
+
+        if not k.isalpha():
+            continue
+
+        single_dict_item = {"char": k, "num": char_dict[k]}
+        
+        dict_list.append(single_dict_item)
+
+
+    dict_list.sort(key = lambda x: x["num"], reverse = True)
+    return dict_list
